bb7179686a02bfc1b051e1e3d0526ea1aa9890b8
Yaroslav Semenov <yaryk2013@gmail.com>
2022-01-07 23:01:39 +0200

    completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', 'disabled');
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+}
